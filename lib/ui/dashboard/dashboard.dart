8972e2b3bd67c7c8a8d6ba551307bc1793abc167
Sifatsb <sifatsb1906@gmail.com>
2023-01-28 22:27:00 +0600

    could not complete this task

--- lib/ui/dashboard/dashboard.dart
+++ lib/ui/dashboard/dashboard.dart
@@ -41,76 +41,129 @@ class _DashboardState extends State<Dashboard> {
           return true;
         }
       },
-      child: SafeArea(
-          child: Obx(() => internetController.internet == true ? Scaffold(
-              backgroundColor: AppColors.secondaryColor,
-              appBar: AppBar(
-                title: const Text('Owl Sports', style: AppStyle.title3,),
-                centerTitle: true,
-                backgroundColor: AppColors.primaryColor,
-                elevation: 3,
+      child: Scaffold(
+        backgroundColor: AppColors.primaryColor,
+          appBar: AppBar(
+            title: const Text('Owl Sports', style: AppStyle.title3,),
+            centerTitle: true,
+            backgroundColor: AppColors.primaryColor,
+            elevation: 3,
+          ),
+
+          body: Column(
+            children: [
+
+              Expanded(
+                child: ListView.builder(
+                  reverse: true,
+                  itemCount: 10,
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+
+                    return
+                      const Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Text('Hello world', textAlign: TextAlign.right, style: TextStyle(color: AppColors.textColor, fontSize: 16),),
+                      );
+
+                  },
+                ),
               ),
-              body: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Stack(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(5.0),
-                                child: SizedBox(
-                                  width: w*0.65,
-                                  child: AppWidgets().msgInputField(context,
-                                      controller: textController,
-                                      minLine: 1,
-                                      maxLine: 3,
-                                      hintText: 'Type Message',
-                                      icon: AppWidgets().iconButton(context,
-                                          press: (){},
-                                          icon: const Icon(Icons.emoji_emotions_outlined, color: AppColors.secondaryColor,) )),
 
 
+              Card(
+                color: Colors.white54,
+                child: Row(
+                  children: [
+
+                    AppWidgets().iconButton(context, press: (){
+
+                    }, icon: const Icon(Icons.arrow_forward_ios_outlined, color: AppColors.secondaryColor,)),
+
+                    Expanded(
+                      child: Stack(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: SizedBox(
+                                width: w*0.65,
+                                child: AppWidgets().msgInputField(context,
+                                    controller: textController,
+                                    minLine: 1,
+                                    maxLine: 3,
+                                    hintText: 'Aa',
+                                    icon: AppWidgets().iconButton(context,
+                                        press: (){},
+                                        icon: const Icon(Icons.emoji_emotions_outlined, color: AppColors.secondaryColor,) )),
+
+
+                              ),
+                            ),
+                            Positioned(
+                              right: 32,
+                              bottom: 1,
+                              child: GestureDetector(
+                                onTap: (){
+                                },
+                                child: Container(
+                                    height: h*0.05,
+                                    width: h*0.05,
+                                    padding: const EdgeInsets.all(8),
+                                    margin: const EdgeInsets.only(right: 10, bottom: 8),
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(100),
+                                        color: AppColors.secondaryColor,
+                                        boxShadow: [
+                                          BoxShadow(
+                                              color: Colors.grey.withOpacity(.35),
+                                              blurRadius: 2,
+                                              spreadRadius: 1.5,
+                                              offset: const Offset(0, 2.0))
+                                        ]),
+                                    child: const Icon(Icons.thumb_up_alt_outlined, color: Colors.white,)
                                 ),
                               ),
-                              Positioned(
-                                right: -10,
-                                bottom: 1,
-                                child: GestureDetector(
-                                  onTap: (){
-
-                                  },
-                                  child: Container(
-                                      height: h*0.05,
-                                      width: h*0.05,
-                                      padding: const EdgeInsets.all(8),
-                                      margin: const EdgeInsets.only(right: 10, bottom: 8),
-                                      decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(100),
-                                          color: AppColors.secondaryColor,
-                                          boxShadow: [
-                                            BoxShadow(
-                                                color: Colors.grey.withOpacity(.35),
-                                                blurRadius: 2,
-                                                spreadRadius: 1.5,
-                                                offset: const Offset(0, 2.0))
-                                          ]),
-                                      child: const Icon(Icons.send, color: Colors.white,)
-                                  ),
+                            ),
+                            Positioned(
+                              right: -10,
+                              bottom: 1,
+                              child: GestureDetector(
+                                onTap: (){
+                                },
+                                child: Container(
+                                    height: h*0.05,
+                                    width: h*0.05,
+                                    padding: const EdgeInsets.all(8),
+                                    margin: const EdgeInsets.only(right: 10, bottom: 8),
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(100),
+                                        color: AppColors.secondaryColor,
+                                        boxShadow: [
+                                          BoxShadow(
+                                              color: Colors.grey.withOpacity(.35),
+                                              blurRadius: 2,
+                                              spreadRadius: 1.5,
+                                              offset: const Offset(0, 2.0))
+                                        ]),
+                                    child: const Icon(Icons.send, color: Colors.white,)
                                 ),
                               ),
-                            ]
-                        ),
+                            ),
+                          ]
                       ),
-                    ],
-                  )
-                ],
+                    ),
+
+                  ],
+                ),
               ),
 
 
-          ) : Center(child: Container(height: h, color: Colors.white, child: Lottie.asset('assets/images/no_internet.json'))))),
+            ],
+          )
+
+      ),
     );
   }
 }
